dd31deb4620895c71053c143362580aac895ecbc
yunxuan0501 <127665962+yunxuan0501@users.noreply.github.com>
2023-03-29 09:44:36 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,6 @@
 
 
 const urls = [
-	'https://forms.gle/61uPsZsn6dnXpRMb8',
 	'https://forms.gle/UqYBvC6dNp8uekxSA',
 	'https://forms.gle/pbZudow5ezKZQ7RM9',
 	'https://forms.gle/RxTyJEgZRZ4Gjwqd6',
